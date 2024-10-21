ec1d72e643aad7a1c7ec6d7e61dc0b6114915dcb
just X // Developer OxyHub <76210400+OxyHub-Team@users.noreply.github.com>
2024-10-21 21:58:26 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,14 +1,14 @@
 getgenv().SecureMode = true
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local ArrayField = loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-Interface/ArrayField/main/Source.lua'))()
 
-local Window = Rayfield:CreateWindow({
-    Name = "Rayfield Example Window",
-    LoadingTitle = "Rayfield Interface Suite",
-    LoadingSubtitle = "by Sirius",
+local Window = ArrayField:CreateWindow({
+    Name = "ArrayField Example Window",
+    LoadingTitle = "ArrayField Interface Suite",
+    LoadingSubtitle = "by Arrays",
     ConfigurationSaving = {
        Enabled = true,
-       FolderName = TESTOVIY, -- Create a custom folder for your hub/game
-       FileName = "Big Hub"
+       FolderName = Testoviy, -- Create a custom folder for your hub/game
+       FileName = "ArrayField"
     },
     Discord = {
        Enabled = false,
@@ -20,15 +20,22 @@ local Window = Rayfield:CreateWindow({
        Title = "Untitled",
        Subtitle = "Key System",
        Note = "No method of obtaining the key is provided",
-       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+       FileName = "Key", -- It is recommended to use something unique as other scripts using ArrayField may overwrite your key file
        SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like ArrayField to get the key from
+       Actions = {
+             [1] = {
+                 Text = 'Click here to copy the key link <--',
+                 OnPress = function()
+                     print('Pressed')
+                 end,
+                 }
+             },
        Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
     }
  })
 
  local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
-
  local Toggle = Tab:CreateToggle({
     Name = "Toggle Example",
     CurrentValue = false,
@@ -45,5 +52,4 @@ local Window = Rayfield:CreateWindow({
         print(Value)
     end,
  })
-
- Rayfield:LoadConfiguration()
+ ArrayField:LoadConfiguration()
