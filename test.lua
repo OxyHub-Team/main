76b8469ddab960b8d8f0b2293934ace3dba8a58b
just X // Developer OxyHub <76210400+OxyHub-Team@users.noreply.github.com>
2024-10-21 21:24:59 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,37 +1,36 @@
-local Arrayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/OxyHub-Team/main/refs/heads/main/lualib'))()
-local Window = Arrayfield:CreateWindow({
+getgenv().SecureMode = true
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
     Name = "Rayfield Example Window",
     LoadingTitle = "Rayfield Interface Suite",
     LoadingSubtitle = "by Sirius",
     ConfigurationSaving = {
        Enabled = true,
-       FolderName = TestHUI, -- Create a custom folder for your hub/game
-       FileName = "Big Hub2"
+       FolderName = TESTOVIY, -- Create a custom folder for your hub/game
+       FileName = "Big Hub"
     },
     Discord = {
        Enabled = false,
-       Invite = "sirius", -- The Discord invite code, do not include discord.gg/
+       Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
        RememberJoins = true -- Set this to false to make them join the discord every time they load it up
     },
     KeySystem = false, -- Set this to true to use our key system
     KeySettings = {
-       Title = "Sirius Hub",
+       Title = "Untitled",
        Subtitle = "Key System",
-       Note = "Join the discord (discord.gg/sirius)",
-       FileName = "SiriusKey",
-       SaveKey = true,
+       Note = "No method of obtaining the key is provided",
+       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
        GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-       Key = "Hello"
+       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
     }
  })
+
  local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
- local Tab2 = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
- local Tab3 = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
- local Tab4 = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
 
  local Toggle = Tab:CreateToggle({
     Name = "Toggle Example",
-    Info = "Toggle info/Description.", -- Speaks for itself, Remove if none.
     CurrentValue = false,
     Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
     Callback = function(Value)
@@ -40,11 +39,11 @@ local Window = Arrayfield:CreateWindow({
  })
  local Toggle = Tab:CreateToggle({
     Name = "Toggle Example2",
-    Info = "Toggle info/Description.", -- Speaks for itself, Remove if none.
     CurrentValue = false,
     Flag = "Toggle2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
     Callback = function(Value)
         print(Value)
     end,
  })
- ArrayField:LoadConfiguration()
+
+ Rayfield:LoadConfiguration()
