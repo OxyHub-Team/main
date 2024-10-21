a14e0e75c5ef01a80257f192bff91eceed2e9567
just X // Developer OxyHub <76210400+OxyHub-Team@users.noreply.github.com>
2024-10-21 18:47:45 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,5 +1,4 @@
-local ArrayField = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/MC%3AArrayfield%20Library"))()
-
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/OxyHub-Team/ui-library/refs/heads/main/deflibrary'))()
 local Window = Rayfield:CreateWindow({
     Name = "Rayfield Example Window",
     LoadingTitle = "Rayfield Interface Suite",
@@ -14,7 +13,7 @@ local Window = Rayfield:CreateWindow({
        Invite = "sirius", -- The Discord invite code, do not include discord.gg/
        RememberJoins = true -- Set this to false to make them join the discord every time they load it up
     },
-    KeySystem = true, -- Set this to true to use our key system
+    KeySystem = false, -- Set this to true to use our key system
     KeySettings = {
        Title = "Sirius Hub",
        Subtitle = "Key System",
@@ -25,7 +24,6 @@ local Window = Rayfield:CreateWindow({
        Key = "Hello"
     }
  })
-
  local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
  local Tab2 = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
  local Tab3 = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
@@ -44,7 +42,7 @@ local Window = Rayfield:CreateWindow({
     Name = "Toggle Example2",
     Info = "Toggle info/Description.", -- Speaks for itself, Remove if none.
     CurrentValue = false,
-    Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Flag = "Toggle2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
     Callback = function(Value)
         print(Value)
     end,
